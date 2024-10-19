ee5516d97ade70ae9c3f43a37ae99072174f3847
Juanmanuelnavia <juanmanuelnaviacastillo@gmail.com>
2024-10-18 19:18:55 -0500

    Update codigo.java

--- codigo.java
+++ codigo.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int numeroFinal = 0;
-        int sumaAcumulada = 0;
+        int sumafinal = 0;
         int iterador = 0;
 
         Scanner lector = new Scanner(System.in);
-        System.out.print("Ingrese el número que desea calcular: ");
+        System.out.print("porfavor Ingrese el número que desea calcular: ");
         numeroFinal = lector.nextInt();
 
         while (numeroFinal >= iterador) {
-            sumaAcumulada += iterador;
+            sumafinal += iterador;
             iterador++;
         }
 
-        System.out.println("La suma de los primeros " + numeroFinal + " números naturales es igual a " + sumaAcumulada);
+        System.out.println("La suma de los primeros " + numeroFinal + " los números naturales es igual a " + sumafinal);
     }
 }
